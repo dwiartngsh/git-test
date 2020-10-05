de4555083d5f7e5549c9f2d12e07fe9cff524a01
dwiartngsh <dwiartiningsih56@gmail.com>
2020-10-05 10:31:43 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quod corrupti fuga rem ipsum facilis eius quibusdam deleniti molestias veritatis ipsam voluptas delectus voluptatibus quis, earum quasi nulla nemo recusandae. </p>
+    <ul>
+    <li>list 1</li>
+    <li>list 2 </li>
+    </ul>
 </body>
 </html>
